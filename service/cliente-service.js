fd9a1c7da449a2de3e010b7d3d97918fc01dca9b
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-09-22 22:01:47 -0300

    aplicando o try e o catch para listar erros

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -2,7 +2,10 @@
 const listaClientes = () => {  
     return fetch(`http://localhost:3000/profile`)
     .then( resposta => {
-        return resposta.json()        
+        if (resposta.ok) {
+            return resposta.json() 
+        }
+         throw new Error('Não foi possível listar os clientes')     
     })   
 } 
 
@@ -18,21 +21,30 @@ const criaCliente = (nome, email) => {
         })
     })
     .then(resposta=> {
-        return resposta.body
+        if(resposta.ok){
+            return resposta.body
+        }        
+        throw new Error('Não foi possível criar um cliente clientes') 
     })
 }
 
 const removeCliente = (id)=> {
     return fetch(`http://localhost:3000/profile/${id}`, {
         method: 'DELETE'
+    }).then( resposta => {
+        if(!resposta.ok){
+            throw new Error('Não foi possível remover um cliente') 
+        }
     })
-
 }
 
 const detalhaCliente = (id)=> {
     return fetch(`http://localhost:3000/profile/${id}`)
     .then( resposta => {
-        return resposta.json()        
+        if(resposta.ok) {
+            return resposta.json()
+        }
+        throw new Error('Não foi possível detalhar um cliente')         
     }) 
 }
 
@@ -48,7 +60,10 @@ const atualizaCliente = (id, nome, email) => {
         })
     })
     .then( resposta => {
-        return resposta.json()
+        if(resposta.ok) {
+            return resposta.json()
+        }
+        throw new Error('Não foi possível atualizar um cliente') 
     })
 }
 
